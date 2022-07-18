6568f98282e9b18988f70adf483b4570a493cca2
buffetng <ulaitorcodes@gmail.com>
2022-07-18 16:03:10 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+const Home = () => {
+  return (
+    <>
+        <Sidebar/>
+        <Navbar/>
+    </>
+
+  )
+}
+
+export default Home
